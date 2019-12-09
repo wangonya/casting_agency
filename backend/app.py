1ae8941702c2eb2e8e40a2687191412dc1a46685
wangonya <kwangonya@gmail.com>
2019-12-09 12:13:51 +0300

    add error handling methods

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+from auth import AuthError
 
 app = Flask(__name__)
 
@@ -6,6 +7,53 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+# Error Handling
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "not found"
+    }), 404
+
+
+@app.errorhandler(409)
+def duplicate(error):
+    return jsonify({
+        "success": False,
+        "error": 409,
+        "message": "duplicate"
+    }), 409
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error
+    }), error.status_code
 
 
 if __name__ == '__main__':

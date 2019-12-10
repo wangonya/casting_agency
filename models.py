679e12a641d6e415baf87651424fb5106047aefb
wangonya <kwangonya@gmail.com>
2019-12-10 15:20:57 +0300

    restructure

---
+++ models.py
@@ -0,0 +1,67 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
+
+db = SQLAlchemy()
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+class Movies(db.Model):
+    """Movies model
+    records must have a unique title
+    records must also have a release_date
+    """
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    title = Column(String(80), unique=True, required=True)
+    release_date = Column(Integer(), required=True)
+
+    def insert(self):
+        """inserts a new record into the Movies db
+        EXAMPLE
+            movie = Movies(title=title, release_date=release_date)
+            movie.insert()
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.delete()
+    '''
+
+    def delete(self):
+        """deletes a record from the Movies"""
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink.query.filter(Drink.id == id).one_or_none()
+            drink.title = 'Black Coffee'
+            drink.update()
+    '''
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.short())
